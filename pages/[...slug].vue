56cc233d04c3468957277a14f6bb5d4056f68425
stijns96 <47820758+stijns96@users.noreply.github.com>
2022-09-23 13:21:28 +0200

    Change pages to [...slug].vue

---
+++ pages/[...slug].vue
@@ -0,0 +1,9 @@
+<script setup>
+let { slug } = useRoute().params;
+const path = slug ? slug.join("/") : "home";
+const story = await useStoryblok(path, { version: "draft" });
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
