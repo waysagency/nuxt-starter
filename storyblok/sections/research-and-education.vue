13dbf9fedb026e7d7874e82b8c242cd69a80ce39
stijns96 <47820758+stijns96@users.noreply.github.com>
2022-09-25 19:10:31 +0200

    Create research-and-education.vue

---
+++ storyblok/sections/research-and-education.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { blok } = defineProps({ blok: Object });
+
+const sectionOptions = reactive({
+  heading: blok.heading,
+  intro: blok.text,
+  buttonLabel: blok.button_label,
+  buttonLink: blok.button_link,
+});
+</script>
+
+<template>
+  <format-section v-editable="blok" :options="sectionOptions">
+    <StoryblokComponent v-for="blok in blok.blocks" :key="blok._uid" :blok="blok" />
+  </format-section>
+</template>
