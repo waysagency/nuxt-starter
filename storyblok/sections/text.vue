e07876b18ec3a7b681049f0b983a77485b491a9c
stijns96 <47820758+stijns96@users.noreply.github.com>
2022-09-26 20:02:35 +0200

    Add text section

---
+++ storyblok/sections/text.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { blok } = defineProps({ blok: Object });
+
+const sectionOptions = reactive({
+  heading: blok.heading,
+  intro: blok.text,
+});
+</script>
+
+<template>
+  <format-section :options="sectionOptions" />
+</template>
