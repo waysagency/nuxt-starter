68df125c3110e82cd23d32ebb798ec5fb9a2053c
stijns96 <47820758+stijns96@users.noreply.github.com>
2022-09-27 20:38:50 +0200

    Update projects.vue

--- storyblok/sections/projects.vue
+++ storyblok/sections/projects.vue
@@ -95,7 +95,11 @@ const sectionOptions = reactive({
         tag="div"
         class="col-span-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:col-span-12"
       >
-        <project-card v-for="(project, index) in projects" :key="index" :project="project" />
+        <project-card
+          v-for="(project, index) in projects.slice(0, 8)"
+          :key="index"
+          :project="project"
+        />
       </TransitionGroup>
     </div>
     <div v-else>
