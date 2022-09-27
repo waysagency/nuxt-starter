5cbd3a50dad1ee462e8fc8e617d6390f3998880b
stijns96 <47820758+stijns96@users.noreply.github.com>
2022-09-27 19:50:19 +0200

    Create project

---
+++ storyblok/blocks/extra-project-row.vue
@@ -0,0 +1,7 @@
+<script setup>
+defineProps({ blok: Object });
+</script>
+
+<template>
+  <format-rich-text v-editable="blok" :text="blok.text" class="grid" />
+</template>
---
+++ storyblok/content-types/project.vue
@@ -0,0 +1,80 @@
+<script setup>
+const { story, blok } = defineProps({ story: Object, blok: Object });
+
+const sectionOptionsHero = reactive({
+  containerClass: "w-full my-0 h-[500px]",
+  containerMainClass: "space-y-0 flex items-center",
+});
+
+const sectionOptionsAbout = reactive({
+  heading: "About the project",
+  intro: blok.about_text,
+});
+
+const sectionOptionsResearch = reactive({
+  heading: "Experimental research",
+  intro: blok.research_text,
+});
+
+// const about_text = computed(() => renderRichText(blok.about_text));
+// const about_text_2 = computed(() => renderRichText(blok.about_text_2));
+
+// const storyblokApi = useStoryblokApi();
+// const {
+//   data: { stories: persons },
+// } = await storyblokApi.get("cdn/stories", {
+//   version: "draft",
+//   starts_with: `${blok.about_research_team.cached_url}team`,
+// });
+</script>
+
+<template>
+  <format-section
+    v-editable="blok"
+    class="color-scheme color-scheme--primary"
+    :options="sectionOptionsHero"
+  >
+    <div class="hero__image">
+      <format-image
+        :image="blok.background_image"
+        class="hero__background-image absolute inset-0"
+      />
+      <div
+        class="hero__image-overlay after:absolute after:inset-0 after:bg-primary after:bg-opacity-70"
+      ></div>
+    </div>
+
+    <div
+      class="hero__content container--background container absolute top-1/2 left-1/2 z-10 my-0 -translate-x-1/2 -translate-y-1/2 text-center text-white"
+    >
+      <h1 class="hero__title col-span-4 lg:col-span-12">{{ blok.title || story.name }}</h1>
+      <p class="hero__subtitle col-span-4 lg:col-span-8 lg:col-start-3" v-if="blok.intro">
+        {{ blok.intro }}
+      </p>
+
+      <badges
+        :tag-list="story.tag_list"
+        class="col-span-4 justify-center lg:col-span-10 lg:col-start-2"
+      />
+    </div>
+  </format-section>
+
+  <format-section :options="sectionOptionsAbout">
+    <image-swiper :images="blok.about_images" />
+    <format-rich-text :text="blok.about_text_2" class="grid" />
+  </format-section>
+
+  <format-section :options="sectionOptionsResearch" v-if="blok.research_images.length > 0">
+    <image-swiper :images="blok.research_images" />
+  </format-section>
+
+  <template v-if="blok.extra_rows.length > 0">
+    <format-section
+      v-for="(row, index) in blok.extra_rows"
+      :key="index"
+      :options="{ heading: row.heading }"
+    >
+      <StoryblokComponent :blok="row" />
+    </format-section>
+  </template>
+</template>
